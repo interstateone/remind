7763bcaac667019d8e502a335122bdc401273906
Brandon Evans <brandon@brandonevans.ca>
2014-08-18 22:59:04 -0600

    Initial Commit

---
+++ Remind/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Remind
+//
+//  Created by Brandon Evans on 2014-08-18.
+//  Copyright (c) 2014 Brandon Evans. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
