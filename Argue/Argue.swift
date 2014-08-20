4a7c5cc7ae01876ef70e8bf10332bcfb871c0225
Brandon Evans <brandon@brandonevans.ca>
2014-08-19 20:27:41 -0600

    Extract Argument struct into its own file

--- Argue/Argue.swift
+++ Argue/Argue.swift
@@ -19,52 +19,6 @@ extension String {
     }
 }
 
-public struct Argument {
-    let fullName: String
-    let shortName: String
-    let description: String
-    let isFlag: Bool
-
-    public init(fullName: String, shortName: String, description: String, isFlag: Bool) {
-        self.fullName = fullName
-        self.shortName = shortName
-        self.description = description
-        self.isFlag = isFlag
-    }
-
-    public func matchesArgumentName(argumentName: String) -> Bool {
-        if argumentName.hasPrefix("---") {
-            return false
-        }
-        else if argumentName.hasPrefix("--") {
-            return fullName == argumentName[2..<countElements(argumentName)]
-        }
-        else if argumentName.hasPrefix("-") {
-            let start = argumentName.startIndex
-            return shortName == argumentName[1..<countElements(argumentName)]
-        }
-        return false
-    }
-
-    public func usageString() -> String {
-        return "--\(fullName)\t(-\(shortName))\t\(description)"
-    }
-}
-
-extension Argument: Equatable {}
-
-public func ==(lhs: Argument, rhs: Argument) -> Bool {
-        return lhs.fullName == rhs.fullName
-}
-
-extension Argument: Hashable {
-    public var hashValue: Int {
-        get {
-            return fullName.hashValue
-        }
-    }
-}
-
 public class Argue: NSObject {
     let usage: String
     let arguments: [Argument]
