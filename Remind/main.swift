f8856ea5884031dc39164128c61d7a2431e162c6
Brandon Evans <brandon@brandonevans.ca>
2016-12-30 21:15:50 -0700

    Update to Swift 3, update Argue

--- Remind/main.swift
+++ Remind/main.swift
@@ -9,75 +9,107 @@
 import Foundation
 import EventKit
 
-let listArgument = Argument(type: .Option, fullName: "list", shortName: "l", description: "Prints only the reminders in the given list or creates a new reminder there")
-let newArgument = Argument(type: .Option, fullName: "new", shortName: "n", description: "Creates a new reminder")
+let listArgument = Argument(type: .value, fullName: "list", shortName: "l", description: "Prints only the reminders in the given list or creates a new reminder there")
+let newArgument = Argument(type: .value, fullName: "new", shortName: "n", description: "Creates a new reminder")
 let usage = "A little app to quickly deal with reminders."
 let argue = Argue(usage: usage, arguments: [newArgument, listArgument])
 
-if let error = argue.parse() {
-    println("Error parsing arguments: \(error.localizedDescription)")
+do {
+    try argue.parse()
+}
+catch {
+    print("Error parsing arguments: \(error.localizedDescription)")
     exit(1)
 }
 
 if argue.helpArgument.value != nil {
-    println(argue.description)
+    print(argue.description)
     exit(0)
+}
+
+func calendar(named requestedCalendarName: String, in calendars: [EKCalendar]) -> EKCalendar? {
+    return calendars.filter { $0.title == requestedCalendarName }.first
+}
+
+let ShortDateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .short
+    formatter.doesRelativeDateFormatting = true
+    return formatter
+}()
+
+func description(for reminders: [EKReminder]) -> String {
+    return reminders.enumerated().reduce("") { result, reminder in
+        var description = "#\(reminder.offset + 1)\t\(reminder.element.calendar.title): \(reminder.element.title)"
+
+        if let components = reminder.element.dueDateComponents, let date = Calendar.current.date(from: components) {
+            description += " (due \(ShortDateFormatter.string(from: date)))"
+        }
+
+        return result + description + "\n"
+    }
 }
 
 let store = EKEventStore()
 
-func calendarNamed(requestedCalendar: String?) -> [EKCalendar] {
-    var calendars = store.calendarsForEntityType(EKEntityTypeReminder) as [EKCalendar]
-    if requestedCalendar == nil {
-        return calendars
+store.requestAccess(to: .reminder, completion: { (granted, error) -> Void in
+    guard granted, error == nil else {
+        if let error = error {
+            print("Error fetching reminders: \(error.localizedDescription).")
+        }
+        else if !granted {
+            print("remind doesn't have permission to access your reminders.")
+        }
+        exit(1)
     }
-    calendars = calendars.filter { $0.title == requestedCalendar }
-    return calendars
-}
 
-func printReminders(reminders: [EKReminder]) {
-    for (index, reminder) in enumerate(reminders) {
-        print("#\(index + 1)\t\(reminder.calendar.title): \(reminder.title)")
-        if let components = reminder.dueDateComponents {
-            if let date = NSCalendar.currentCalendar().dateFromComponents(components) {
-                let formatter = NSDateFormatter()
-                formatter.dateStyle = .ShortStyle
-                formatter.timeStyle = .ShortStyle
-                formatter.doesRelativeDateFormatting = true
-                print(" (due \(formatter.stringFromDate(date)))")
-            }
-        }
-        print("\n")
+    let calendars = store.calendars(for: .reminder)
+    guard let firstCalendar = calendars.first else {
+        print("Error: didn't find at least one reminder list.")
+        exit(1)
     }
-}
 
-store.requestAccessToEntityType(EKEntityTypeReminder, completion: { (granted, error) -> Void in
-    if granted && error == nil {
-        let calendars = calendarNamed(listArgument.value as? String)
-
-        // Create new reminder
-        if let title = newArgument.value as? String {
-            let reminder = EKReminder(eventStore: store)
-            reminder.title = title
-            reminder.calendar = calendars.first
-            store.saveReminder(reminder, commit: true, error: nil)
-            exit(0)
-        }
-        // Print reminders
-        else {
-            let reminderPredicate = store.predicateForIncompleteRemindersWithDueDateStarting(nil, ending: nil, calendars: calendars)
-            store.fetchRemindersMatchingPredicate(reminderPredicate, completion: { r in
-                var reminders = r as [EKReminder]
-                reminders.sort { $0.calendar.title > $1.calendar.title }
-                printReminders(reminders)
-                exit(0)
-            })
-        }
+    let specifiedCalendar: EKCalendar?
+    if let calendarName = listArgument.value as? String {
+        specifiedCalendar = calendar(named: calendarName, in: calendars)
     }
     else {
-        println("Error fetching reminders: \(error.localizedDescription)")
-        exit(1)
+        specifiedCalendar = nil
+    }
+
+    // Create new reminder
+    if let title = newArgument.value as? String {
+        let reminder = EKReminder(eventStore: store)
+        reminder.title = title
+        reminder.calendar = specifiedCalendar ?? firstCalendar
+
+        do {
+            try store.save(reminder, commit: true)
+        }
+        catch {
+            print(error)
+            exit(1)
+        }
+
+        exit(0)
+    }
+    // Print reminders
+    else {
+        let specifiedCalendars: [EKCalendar]
+        if let specifiedCalendar = specifiedCalendar {
+            specifiedCalendars = [specifiedCalendar]
+        }
+        else {
+            specifiedCalendars = calendars
+        }
+        let reminderPredicate = store.predicateForIncompleteReminders(withDueDateStarting: nil, ending: nil, calendars: specifiedCalendars)
+        store.fetchReminders(matching: reminderPredicate, completion: { reminders in
+            let sortedReminders = reminders?.sorted { $0.calendar.title > $1.calendar.title } ?? []
+            print(description(for: sortedReminders))
+            exit(0)
+        })
     }
 })
 
-NSRunLoop.currentRunLoop().run()
+RunLoop.current.run()
